9f624671869346913d1d80d4fa6f85168bb9c63d
marcomafcorp <marcomcse@gmail.com>
2018-01-30 22:00:31 -0800

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 // Create a new componet
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCRZTqtcSlbTnuLvEOcaZ2XthsAgL2msaQ';
+
+YTSearch({key: API_KEY, term: 'React JS'}, function(data) {
+  console.log(data);
+});
+
 
 const App = () => {
   return (
